70fa8f7fa063d0317adc634077dd6a91b4cff8ab
Your Name <you@example.com>
2020-07-09 16:59:18 -0700

    almost done

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<!--
+	Paradigm Shift by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>!Carlitha Johnson | ..~•¥ Royal! €•~.. | Official Page!</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+		<div class="layout-bg">
+			<video src="assets/video/Notway+3D+Green+Wave+Vista+Ultimate+Animated+wallpaper.mp4" autoplay loop muted class="video-bg"></video>
+		</div>
+		<div class="overlay-bg"></div>
+		<div class="background-bg"></div>
+
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+				<section class="intro">
+					<header>
+						<h1>Carlitha Johnson</h1>
+						<p>Reach out! Security Services! <a href="https://twitter.com/ajlkn">@CarlithaJohnson</a> / <a href="tel:510-459-8026">510-459-8026</a></p>
+						<ul class="actions">
+							<li><a href="#first" class="arrow scrolly"><span class="label">Next</span></a></li>
+						</ul>
+					</header>
+					<div class="content">
+						<!-- <span class="image fill" data-position="center"><img src="images/pic01.jpg" alt="" /></span> -->
+					</div>
+				</section>
+
+				<!-- Section -->
+				<section id="first">
+					<header>
+						<h2>Magna sed nullam nisl adipiscing</h2>
+					</header>
+					<div class="content fuckyourapproval-white-text">
+						<p><strong>Lorem ipsum dolor</strong> sit amet consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.</p>
+						<!-- <span class="image main"><img src="images/pic02.jpg" alt="" /></span> -->
+					</div>
+				</section>
+
+				<!-- Copyright -->
+					<div class="copyright">&copy; Carlitha Johnson. All rights reserved. Design: <a href="http://luisconstante.com">Luis G Constante B</a>.</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
